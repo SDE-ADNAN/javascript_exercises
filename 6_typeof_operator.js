529597afa3562c3b4dcf6ab395e31d1ccb07a83b
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-03 15:20:08 +0530

    practiced typeof operator

---
+++ 6_typeof_operator.js
@@ -0,0 +1,7 @@
+console.log(typeof(100))
+console.log(typeof(0.0004))
+console.log(typeof('hello'))
+console.log(typeof([1,2]))
+console.log(typeof(true))
+console.log(typeof(null))
+console.log(typeof(myname))
