3b6024e6887029109e5a0daa0ac65b8fe7fdf023
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-09 19:02:35 +0530

    added 14th exercise

---
+++ 14_comparission_operators.js
@@ -0,0 +1,12 @@
+var x = 5;
+var y = 10;
+var z = "5";
+
+console.log(x == z);
+console.log(x === z);
+console.log(x != z);
+console.log(x !== z);
+console.log(x < z);
+console.log(x > z);
+console.log(x <= z);
+console.log(x >= z);
