d240c2f6a59cb563c5f507eae4e8cdf57684075a
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-09 18:50:08 +0530

    added 13th exercise

---
+++ 13_assignment_operators.js
@@ -0,0 +1,22 @@
+var x = 10;
+console.log(x);
+
+x = 20;
+x +=30;
+console.log(x)
+
+x = 20;
+x -=30;
+console.log(x)
+
+x = 20;
+x *=30;
+console.log(x)
+
+x = 20;
+x /=30;
+console.log(x)
+
+x = 20;
+x %=30;
+console.log(x)
