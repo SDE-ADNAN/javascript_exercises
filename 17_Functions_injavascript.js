cb3fbe0d5fe66b33a4e5b1c53a123d9d46e3a60f
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-09 19:33:01 +0530

    added 17th exercise

---
+++ 17_Functions_injavascript.js
@@ -0,0 +1,19 @@
+//A JavaScript function is a block of code 
+//  designed to perform a particular task.
+// A js function is defined with the
+// function keyword, followed by a name,
+// followed by parentheses ().
+
+
+function sayHello(){
+    console.log("hello people");
+}
+
+
+sayHello();
+
+const hello=()=>{
+    console.log(" hello world from arrow funcs");
+}
+
+hello()
