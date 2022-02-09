b6d1774cbd8a5a2aa212564010bc442546c29385
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-09 20:37:21 +0530

    added 21th exercise

---
+++ 21_function_declaration_and_expression_.js
@@ -0,0 +1,17 @@
+// Function Declaration
+
+function mySum(num1, num2) {
+    return num1 + num2;
+
+}
+
+console.log(mySum(20, 30) + "from Function Declaration")
+
+
+// Function Expression
+
+var mySum = function (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(mySum(10, 20) + "From Function Expression")
