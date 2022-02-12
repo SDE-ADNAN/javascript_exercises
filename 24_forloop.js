f482c103207f292a4ec624dc73a8c219f846b43f
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-12 22:28:23 +0530

    forloop

---
+++ 24_forloop.js
@@ -0,0 +1,12 @@
+// create an array of number
+
+var myNumbers = [1,5,3,40,5,27,5,3,8,19];
+
+
+//display the array elements
+//using for loop
+
+for(var i=0; i<myNumbers.length; i++){
+    console.log(myNumbers[i])
+}
+
