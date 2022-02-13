9d44b857a86ffd100190e1efa956f8bcdcfcc651
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-02-13 09:09:08 +0530

    added for each

---
+++ 25_forEach_loop.js
@@ -0,0 +1,12 @@
+
+// create an array of nums
+var myNumbers = [1,5,3,40,5,27,5,3,8,19];
+
+// display the array elements
+//using forEach loop
+
+myNumbers.forEach((item,i)=>{
+    console.log(item);
+    // console.log(item(i));
+    console.log(i) //this  i is the index no. of the array
+})
